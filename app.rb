c8c9e99f300add5dc1a1aabbc85d8bc9b7f05f66
Alexa <alexa.anderson@outlook.com>
2016-11-17 11:44:20 -0600

    Add pseudocode

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require 'sinatra/reloader'
 require 'pry-byebug'
 
 get '/' do 
-  'Hello, world!'
+  erb :index
+  sleep(2)
+  redirect to("blackjack")
 end
 
+get '/blackjack' do
+  # if session[:player] reinstantiate game
+  # else deal for the player and dealer
+  erb :main
+end
