e849a1f887c191f625d16eee25adcf4dda7cc08f
Alexa <alexa.anderson@outlook.com>
2016-11-17 12:39:32 -0600

    Bets!

--- app.rb
+++ app.rb
@@ -1,21 +1,38 @@
+#!/usr/bin/env ruby
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry-byebug'
 require 'erb'
 
 get '/' do 
-  erb :index 
+  erb :index
 end
 
 get '/blackjack' do
-  # if session[:player] reinstantiate game
+  # unless session[:game] reinstantiate game
+  	# instantiate player bankroll
   # else deal for the player and dealer
   # break down & store
   erb :blackjack
 end
 
+get '/blackjack/bet' do
+	# instantiate game
+	# show bankroll
+	# ask player for bet amount
+	# break down & store
+	erb :bet
+end
+
+post '/blackjack/bet' do
+	# instantiate game
+	# decrement the bankroll
+	# break down & store
+	redirect "/blackjack"
+end
+
 get '/blackjack/hit' do 
-  # instantiate
+  # instantiate game
   # draw from deck 
   # add drawn cardto player's hand 
   # break down & store 
@@ -23,7 +40,7 @@ get '/blackjack/hit' do
 end
 
 get '/blackjack/stay' do 
-  # instantiate 
+  # instantiate game
   # dealer does his turns
   # break down & store
   redirect '/blackjack/results'
