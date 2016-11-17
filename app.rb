07c4deefd595b22013405ce526c91221977e16c4
Pramod Jacob <psjacob68@gmail.com>
2016-11-17 13:13:30 -0500

    working on app.rb

--- app.rb
+++ app.rb
@@ -1,15 +1,36 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry-byebug'
+require 'erb'
 
 get '/' do 
-  erb :index
-  sleep(2)
-  redirect to("blackjack")
+  erb :index 
 end
 
 get '/blackjack' do
   # if session[:player] reinstantiate game
   # else deal for the player and dealer
-  erb :main
+  # break down & store
+  erb :blackjack
 end
+
+get '/blackjack/hit' do 
+  # instantiate
+  # draw from deck 
+  # add drawn cardto player's hand 
+  # break down & store 
+  redirect '/blackjack'
+end
+
+get '/blackjack/stay' do 
+  # instantiate 
+  # dealer does his turns
+  # break down & store
+  redirect '/blackjack/results'
+end
+
+get '/blackjack/results' do 
+  # displays results 
+  # ask to play again 
+end
+
